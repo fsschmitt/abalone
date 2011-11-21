c12d3279a0242bdd89caaeb02b622c00e653af82
Felipe Schmitt <fsschmitt@gmail.com>
2011-11-21 19:59:39 +0000

    regras do jogo, menos o empurrar

---
+++ vdmloop.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set S1=Movimento.rtf
+set S2=Casa.rtf
+set S3=Jogada.rtf
+set S4=Jogo.rtf
+set S5=Jogador.rtf
+set S6=Tabuleiro.rtf
+set S7=TestMovimento.rtf
+
+"C:\Program Files (x86)\The VDM++ Toolbox Academic v8.0\bin\vppde" -p -R vdm.tc %S1% %S2% %S3% %S4% %S5% %S6% %S7%
+for /R %%f in (*.arg) do call vdmtest "%%f"
---
+++ vdmtest.bat
@@ -0,0 +1,22 @@
+@echo off
+rem Tests the date book specification for one test case (argument)
+rem -- Output the argument to stdout (for redirect) and "con" (for user feedback)
+echo VDM Test: '%1' > con
+echo VDM Test: '%1'
+
+rem short names for specification files in Word RTF Format
+set S1=Movimento.rtf
+set S2=Casa.rtf
+set S3=Jogada.rtf
+set S4=Jogo.rtf
+set S5=Jogador.rtf
+set S6=Tabuleiro.rtf
+set S7=TestMovimento.rtf
+
+rem -- Calls the interpreter for this test case
+"C:\Program Files (x86)\The VDM++ Toolbox Academic v8.0\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1  %S1% %S2% %S3% %S4% %S5% %S6% %S7%
+
+rem -- Check for difference between result of execution and expected result.
+if EXIST %1.exp fc /w %1.res %1.exp
+
+:end
