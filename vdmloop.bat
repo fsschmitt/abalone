ad5560b3275abb615ef623bd8dbf4411c984a851
Felipe Schmitt <fsschmitt@gmail.com>
2011-11-23 09:24:00 +0000

    Testes do jogo, init

--- vdmloop.bat
+++ vdmloop.bat
@@ -6,7 +6,7 @@ set S3=Jogada.rtf
 set S4=Jogo.rtf
 set S5=Jogador.rtf
 set S6=Tabuleiro.rtf
-set S7=TestMovimento.rtf
+set S8=TestJogo.rtf
 
-"C:\Program Files (x86)\The VDM++ Toolbox Academic v8.0\bin\vppde" -p -R vdm.tc %S1% %S2% %S3% %S4% %S5% %S6% %S7%
+"C:\Program Files (x86)\The VDM++ Toolbox Academic v8.0\bin\vppde" -p -R vdm.tc %S1% %S2% %S3% %S4% %S5% %S6% %S8%
 for /R %%f in (*.arg) do call vdmtest "%%f"
--- vdmtest.bat
+++ vdmtest.bat
@@ -11,10 +11,10 @@ set S3=Jogada.rtf
 set S4=Jogo.rtf
 set S5=Jogador.rtf
 set S6=Tabuleiro.rtf
-set S7=TestMovimento.rtf
+set S8=TestJogo.rtf
 
 rem -- Calls the interpreter for this test case
-"C:\Program Files (x86)\The VDM++ Toolbox Academic v8.0\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1  %S1% %S2% %S3% %S4% %S5% %S6% %S7%
+"C:\Program Files (x86)\The VDM++ Toolbox Academic v8.0\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1  %S1% %S2% %S3% %S4% %S5% %S6% %S8%
 
 rem -- Check for difference between result of execution and expected result.
 if EXIST %1.exp fc /w %1.res %1.exp
